c2765843f532a88155212b97a9d625105d962300
Abdur Rahman Emon <aremon86851@gmail.com>
2022-10-01 08:23:16 +0600

    added break seconds

--- src/component/BreakTime/BreakTime.js
+++ src/component/BreakTime/BreakTime.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './BreakTime.css'
 
 const BreakTime = () => {
-    let buttons = document.querySelectorAll('button');
+    let buttons = document.querySelectorAll('#clickable button');
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             buttons.forEach(btn => btn.classList.remove('active'))
             this.classList.add('active')
-            // const getSeconds = document.getElementById('break-time-seconds');
-            // getSeconds.innerText = this.innerText
+            const getSeconds = document.getElementById('break-time-seconds');
+            getSeconds.innerText = this.innerText
+
         })
     })
+
     return (
         <div className='break-container'>
             <h2>Add A Break</h2>
