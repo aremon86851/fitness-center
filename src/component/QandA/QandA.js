36bf1d275b08127175ad6577b90b4ece608c9190
Abdur Rahman Emon <aremon86851@gmail.com>
2022-10-01 16:20:07 +0600

    responsive qna section complete

--- src/component/QandA/QandA.js
+++ src/component/QandA/QandA.js
@@ -9,6 +9,17 @@ const QandA = () => {
                     <h2>Q: How does react work?</h2>
                     <p><span style={{ fontWeight: 'bold' }}>A:</span> ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props.</p>
                 </div>
+                <div>
+                    <h2>Q: Difference between props and state?</h2>
+                    <p><span style={{ fontWeight: 'bold' }}>A:</span>
+                        <span>PROPS: </span>Props are used to pass data from one component to another.<br />
+                        <span>STATE: </span>The state is a local data storage that is local to the component only and cannot be passed to other components.
+                    </p>
+                </div>
+                <div>
+                    <h2>Q: What is use of useEffect in react except data load from api?</h2>
+                    <p><span style={{ fontWeight: 'bold' }}>A:</span>Actually useEffect Hook is to eliminate the side-effects of using class-based components. For example, tasks like updating the DOM, fetching data from API end-points, setting up subscriptions or timers, etc can be lead to unwarranted side-effects.</p>
+                </div>
             </div>
         </div>
     );
